b09638c353da45c25d42279dc0027cabd60bcda3
PiShrestha <pratikstha980@gmail.com>
2025-12-17 16:49:44 -0500

    Initial Spring Boot project setup

---
+++ src/test/java/com/shrestha/wcsimulator/wcsimulator/WcSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shrestha.wcsimulator.wcsimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WcSimulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
